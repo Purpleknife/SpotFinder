065dc934b0cb760337fdfc4858e589901cdccf7b
Hind <hind.nayhi@gmail.com>
2022-11-26 17:40:03 -0700

    Add maps to populate the app.

---
+++ back-end/db/seeds/02_maps.sql
@@ -0,0 +1,8 @@
+INSERT INTO coordinates (creator, date_created, title, city, province, country, latitude, longitude)
+
+VALUES (1, '2022-11-01', 'Best Places to Eat Around Town', 'Toronto', 'Ontario', 'Canada', 43.755839, -79.420263),
+(1, '2021-12-20', 'Best Mountains to Hike', 'Calgary', 'Alberta', 'Canada', 51.043580, -114.047625),
+(1, '2022-08-10', 'Best Parks to visit', 'Calgary', 'Alberta', 'Canada', 51.043580, -114.047625),
+(2, '2022-10-14', 'Best Spas Around Town', 'Toronto', 'Ontario', 'Canada', 43.755839, -79.420263),
+(3, '2022-11-12', 'Best Beach Parks Around Town', 'Vancouver', 'British Columbia', 'Canada', 49.256811, -123.103453),
+(1, '2022-09-13', 'Best Dog Parks', 'Calgary', 'Alberta', 'Canada', 51.043580, -114.047625);
