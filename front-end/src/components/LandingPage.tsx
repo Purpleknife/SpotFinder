8f7c957399b88ad756829d007c6e1bfb1733646e
Hind <hind.nayhi@gmail.com>
2022-11-26 19:06:24 -0700

    Add an axios request to load all maps from db.

--- front-end/src/components/LandingPage.tsx
+++ front-end/src/components/LandingPage.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import NavBar from "./Navbar";
 import Map from './Map';
 
+import axios from 'axios';
+
 const LandingPage = () => {
+
+  const loadAllMaps = () => {
+    return axios.get('/maps')
+      .then((res) => {
+        console.log('maps from front-end', res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    loadAllMaps();
+  }, []);
+
+
   return (
     <div className='landing_page'>
       <NavBar />
