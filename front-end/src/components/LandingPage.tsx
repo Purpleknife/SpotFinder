7505d99ac1d5eff7750b5b427e9e603dbb431d30
Hind <hind.nayhi@gmail.com>
2022-11-26 19:23:18 -0700

    Load all maps from db in Landing Page.

--- front-end/src/components/LandingPage.tsx
+++ front-end/src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NavBar from "./Navbar";
 import Map from './Map';
@@ -6,16 +6,57 @@ import Map from './Map';
 import axios from 'axios';
 
 const LandingPage = () => {
+  const [mapList, setMapList] = useState<any>(null);
+  const [mapData, setMapData] = useState<any>(null);
 
-  const loadAllMaps = () => {
+  const loadAllMaps = async() => {
     return axios.get('/maps')
       .then((res) => {
         console.log('maps from front-end', res.data);
+        setMapData(res.data);
       })
       .catch((error) => {
         console.log(error.message);
       });
   };
+
+  interface Map {
+    id: number;
+    creator: number;
+    date_created: string;
+    title: string;
+    city: string;
+    province: string;
+    country: string;
+    latitude: number;
+    longitude: number;
+  }
+
+  const generateMapList = () => {
+    const allMaps = mapData.map((mp: Map) => {
+      return (
+        <Map
+          key={mp.id}
+          id={mp.id}
+          creator={mp.creator}
+          date_created={mp.date_created}
+          title={mp.title}
+          city={mp.city}
+          province={mp.province}
+          country={mp.country}
+          latitude={mp.latitude}
+          longitude={mp.longitude}
+        />
+      )
+    });
+    setMapList(allMaps);
+  };
+
+  useEffect(() => {
+    if (mapData) {
+      generateMapList();
+    }
+  }, [mapData]);
 
   useEffect(() => {
     loadAllMaps();
@@ -27,7 +68,7 @@ const LandingPage = () => {
       <NavBar />
 
       <div className='map_container'>
-        <Map />
+        {mapList}
       </div>
 
     </div>
--- front-end/src/components/Map.tsx
+++ front-end/src/components/Map.tsx
@@ -3,25 +3,36 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.scss';
 
 import 'leaflet/dist/leaflet.css';
-import L from "leaflet";
+//import L from "leaflet";
 
+interface MapProps {
+  id: number;
+  creator: number;
+  date_created: string;
+  title: string;
+  city: string;
+  province: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+}
 
-const Map = () => {
+const Map = (props: MapProps) => {
 
-  const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+  //const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
 
   return (
     <div className='map'>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[props.latitude, props.longitude]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]} icon={icon}>
+        {/* <Marker position={[51.505, -0.09]} icon={icon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
       </MapContainer>
     </div>
   );
