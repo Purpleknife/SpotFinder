f2880a3bd66a0bb50fa12fc62d34e0ca3970de93
Hind <hind.nayhi@gmail.com>
2022-11-26 17:21:42 -0700

    Add a map to Landing Page.

--- front-end/src/components/LandingPage.tsx
+++ front-end/src/components/LandingPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import NavBar from "./Navbar";
-
+import Map from './Map';
 
 const LandingPage = () => {
   return (
     <div className='landing_page'>
       <NavBar />
+      <Map />
     </div>
   );
 }
---
+++ front-end/src/components/Map.tsx
@@ -0,0 +1,30 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import './Map.scss';
+
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+
+const Map = () => {
+
+  const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+
+
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]} icon={icon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+ 
+export default Map;
+
