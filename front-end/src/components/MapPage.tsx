50e6b8c2487defc5559f0162bfeaa15f34c04252
Hind <hind.nayhi@gmail.com>
2022-11-28 16:02:11 -0700

    Update the value that sends the pins.

--- front-end/src/components/MapPage.tsx
+++ front-end/src/components/MapPage.tsx
@@ -1,6 +1,5 @@
 import {useLocation} from 'react-router-dom';
 
-import React from 'react';
 import MapView from './MapView';
 
 const MapPage = () => {
@@ -8,14 +7,14 @@ const MapPage = () => {
 
   console.log('location', location.state)
   return (
-  <>
-    <MapView 
-      key={location.state.key}
-      id={location.state.id}
-      latitude={location.state.latitude}
-      longitude={location.state.longitude}
-      pins={location.state.pins} />
-  </>
+    <>
+      <MapView 
+        key={location.state.key}
+        id={location.state.id}
+        latitude={location.state.latitude}
+        longitude={location.state.longitude}
+        allPins={location.state.allPins} />
+    </>
   );
 }
  
