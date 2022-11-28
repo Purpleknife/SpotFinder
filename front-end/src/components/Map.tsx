d57c6aa9c6cf32ed0efd2e0ed63109a02ace8850
Hind <hind.nayhi@gmail.com>
2022-11-28 16:01:45 -0700

    Send pins prop with useNavigate.

--- front-end/src/components/Map.tsx
+++ front-end/src/components/Map.tsx
@@ -1,14 +1,10 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-
 import './Map.scss';
 
 import 'leaflet/dist/leaflet.css';
 
-import Pins from './Pins';
-
 import MapView from './MapView';
 
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface MapProps {
   id: number;
@@ -27,31 +23,9 @@ interface MapProps {
 const Map = (props: MapProps) => {
   const navigate = useNavigate();
 
-  const pinsForMaps = props.pins.map((pin) => {
-    //console.log('PIN', pin);
-    return (
-      <Pins
-        key={pin.id}
-        id={pin.id}
-        latitude={pin.latitude}
-        longitude={pin.longitude}
-        title={pin.title}
-        description={pin.description}
-        image={pin.image}
-      />
-    )
-  })
-
   return (
     <div className='map'>
-      <MapView id={props.id} key={props.id} latitude={props.latitude} longitude={props.longitude} pins={pinsForMaps}/>
-      {/* <MapContainer center={[props.latitude, props.longitude]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {pinsForMaps}
-      </MapContainer> */}
+      <MapView id={props.id} key={props.id} latitude={props.latitude} longitude={props.longitude} allPins={props.pins}/>
       
       <div className='map_info'>
         Title: {props.title}
@@ -63,15 +37,17 @@ const Map = (props: MapProps) => {
 
 
       <button type='submit' onClick={() => navigate(`/maps/${props.id}`, 
-        { state: 
-            {id: props.id, 
+        { state: {
+            id: props.id, 
             key: props.id,
             latitude: props.latitude,
-            longitude: props.longitude}
-        })
-            }>
-              
-              View</button>
+            longitude: props.longitude,
+            allPins: props.pins
+          }
+        }
+      )}>
+        View
+      </button>
     </div>
   );
 }
