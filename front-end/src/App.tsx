0eb737f14a75e7a79aa843ab8fb95e4dfa1bf7c2
Hind <hind.nayhi@gmail.com>
2022-11-28 15:03:39 -0700

    Add route that shows the map full screen.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,14 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
+import MapPage from './components/MapPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/maps/:map_id' element={<MapPage />} />
       </Routes>
     </BrowserRouter>
   );
