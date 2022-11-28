a9fe147cf9ae4d811156679631e09ecd9ceaae5e
Hind <hind.nayhi@gmail.com>
2022-11-28 16:01:22 -0700

    Move the logic that loads the pins for each map to <MapView>.

--- front-end/src/components/MapView.tsx
+++ front-end/src/components/MapView.tsx
@@ -2,22 +2,43 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 import './MapView.scss';
 
+import Pins from './Pins';
+
 interface MapViewProps {
   id: number;
   key: number;
   latitude: number;
   longitude: number;
-  pins: any[];
+  allPins: any[];
 }
 
 const MapView = (props: MapViewProps) => {
+
+  const pinsForMaps = props.allPins.map((pin) => {
+    //console.log('PIN', pin);
+    return (
+      <Pins
+        key={pin.id}
+        id={pin.id}
+        latitude={pin.latitude}
+        longitude={pin.longitude}
+        title={pin.title}
+        description={pin.description}
+        image={pin.image}
+      />
+    )
+  });
+
+
   return (
     <MapContainer center={[props.latitude, props.longitude]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-    {props.pins}
+
+      {pinsForMaps}
+
     </MapContainer>
   );
 }
