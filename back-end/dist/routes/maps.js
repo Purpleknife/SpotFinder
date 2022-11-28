843911734ae33920575473271476e9f338851838
Hind <hind.nayhi@gmail.com>
2022-11-28 12:39:25 -0700

    Fix query that loads the maps: now it loads the pins of each map as an array.

--- back-end/dist/routes/maps.js
+++ back-end/dist/routes/maps.js
@@ -10,10 +10,28 @@ const router = express_1.default.Router();
 module.exports = (db) => {
     // Get ALL maps from db:
     router.get('/maps', (req, res) => {
-        const queryString = `SELECT * FROM maps;`;
+        const queryString = `SELECT maps.*, array_to_json(array_agg(pins)) AS pins FROM maps
+      JOIN pins ON map_id = maps.id
+      GROUP BY maps.id;`;
         db.query(queryString)
             .then((data) => {
             console.log('Get MAPS', data.rows);
+            res.json(data.rows);
+        })
+            .catch((error) => {
+            console.log(error.message);
+        });
+    });
+    // Get ALL pins for a specific map:
+    router.get('/pins/:map_id', (req, res) => {
+        const map_id = req.params.map_id;
+        const queryParams = [map_id];
+        const queryString = `
+      SELECT * FROM pins
+      WHERE map_id = $1;`;
+        db.query(queryString, queryParams)
+            .then((data) => {
+            console.log('Get Pins', data.rows);
             res.json(data.rows);
         })
             .catch((error) => {
--- back-end/routes/maps.ts
+++ back-end/routes/maps.ts
@@ -8,11 +8,36 @@ module.exports = (db: any) => {
 
   // Get ALL maps from db:
   router.get('/maps', (req: Request, res: Response) => {
-    const queryString: string = `SELECT * FROM maps;`;
+    const queryString: string = 
+      `SELECT maps.*, array_to_json(array_agg(pins)) AS pins FROM maps
+      JOIN pins ON map_id = maps.id
+      GROUP BY maps.id;`
+      ;
 
     db.query(queryString)
       .then((data: any) => {
         console.log('Get MAPS', data.rows);
+        res.json(data.rows);
+      })
+      .catch((error: Error) => {
+        console.log(error.message);
+      });
+  });
+
+
+  // Get ALL pins for a specific map:
+  router.get('/pins/:map_id', (req: Request, res: Response) => {
+    const map_id: string | number = req.params.map_id;
+
+    const queryParams: (string | number)[] = [map_id];
+    const queryString: string = `
+      SELECT * FROM pins
+      WHERE map_id = $1;`
+      ;
+
+    db.query(queryString, queryParams)
+      .then((data: any) => {
+        console.log('Get Pins', data.rows);
         res.json(data.rows);
       })
       .catch((error: Error) => {
