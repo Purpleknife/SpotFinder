c2b3b5cf6b07a480e3735d67e2d11f75f249168f
Hind <hind.nayhi@gmail.com>
2022-11-28 12:39:51 -0700

    Load the pins of each map.

---
+++ front-end/src/components/Pins.tsx
@@ -0,0 +1,34 @@
+import { Marker, Popup } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+
+interface PinProps {
+  id: number | string;
+  latitude: number;
+  longitude: number;
+  title: string;
+  description: string;
+  image: string;
+}
+
+const Pins = (props: PinProps) => {
+
+  const icon = L.icon({ iconUrl: "/images/marker-icon.png" });
+
+  return (
+    <Marker position={[props.latitude, props.longitude]} icon={icon}>
+      <Popup>
+        {props.title} <br />
+        <img
+          className='pin_img'
+          alt='pin_img'
+          src={props.image}
+        /><br />
+        {props.description}
+      </Popup>
+    </Marker>
+  );
+}
+ 
+export default Pins;
