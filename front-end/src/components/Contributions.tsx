8f116d1a8104652a72f59ce5f015c888c4516ff2
Hind <hind.nayhi@gmail.com>
2022-11-29 13:32:21 -0700

    The user can now see the maps they contributed to in their profile.

---
+++ front-end/src/components/Contributions.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import MapView from './MapView';
+
+import './Contributions.scss';
+
+interface ContributionsProps {
+  id: number;
+  title: string;
+  city: string;
+  province: string;
+  country: string;
+  creator: number;
+  date_created: string;
+  pins: any[];
+  latitude: number;
+  longitude: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+  profile_image: string;
+  user_country: string;
+  user_id: number;
+  user_province: string;
+}
+
+
+const Contributions = (props: ContributionsProps) => {
+  return (
+    <div className='contributions'>
+      <div className='contri_map'>
+        <MapView key={props.id} id={props.id} latitude={props.latitude} longitude={props.longitude} allPins={props.pins} />
+      </div>
+      Title: {props.title}
+      <br />
+      Location: {props.city}, {props.province}, {props.country}
+      <br />
+      Created on {props.date_created}
+    </div>
+  );
+}
+ 
+export default Contributions;
