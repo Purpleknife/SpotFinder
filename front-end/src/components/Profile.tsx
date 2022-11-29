6a164b7af226d4ad207cfec041a287a2ebcae8e8
Hind <hind.nayhi@gmail.com>
2022-11-29 12:47:58 -0700

    Implement axios request to get the profile's data.

--- front-end/src/components/Profile.tsx
+++ front-end/src/components/Profile.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+import { useCookies } from 'react-cookie';
 
 
 const Profile = () => {
+  const [cookies, setCookie] = useCookies(['username', 'user_id', 'logged_in']);
+  const user_id = cookies.user_id;
+  const username = cookies.username;
+
+  const loadProfileData = async() => {
+    return axios.get(`/profile/${user_id}`)
+      .then((res) => {
+        console.log('profile', res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    loadProfileData();
+  }, []);
+
   return (
     <div className='profile'>
-      Hello from Profile.
+      Hello, {username}!
     </div>
   );
 }
