e3480bd053f134fc7e302b57fb51fe028f093a80
Hind <hind.nayhi@gmail.com>
2022-11-29 14:06:20 -0700

    Add contributions to seeds.

---
+++ back-end/db/seeds/04_contributions.sql
@@ -0,0 +1,8 @@
+INSERT INTO contributions (user_id, map_id, date_contibuted, contribution_type)
+
+VALUES (1, 1, '2022-11-01', 'Created Map'),
+(1, 2, '2021-12-20', 'Created Map'),
+(1, 3, '2022-08-10', 'Created Map'),
+(2, 4, '2022-10-14', 'Created Map'),
+(3, 5, '2022-11-12', 'Created Map'),
+(1, 6, '2022-09-13', 'Created Map');
