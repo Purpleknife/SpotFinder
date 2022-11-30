a81d1c8822e5a521ab777b2b642580be55f646e4
Hind <hind.nayhi@gmail.com>
2022-11-30 12:56:04 -0700

    Add some comments to seeds.

---
+++ back-end/db/seeds/06_map_comments.sql
@@ -0,0 +1,5 @@
+INSERT INTO map_comments (user_id, map_id, date_commented, content)
+
+VALUES (2, 1, '2022-11-11', 'Can''t wait to visit these places!'),
+(3, 1, '2021-12-25', 'Needed this!'),
+(1, 2, '2022-08-14', 'I''ll test and let you know!');
