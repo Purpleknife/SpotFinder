2e6dc2f90b2a0005c50ba37741a7d0f68843db51
Hind <hind.nayhi@gmail.com>
2022-11-30 11:34:37 -0700

    Change NavBar layout when a user is logged-in.

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -4,10 +4,21 @@ import './NavBar.scss';
 
 import Button from 'react-bootstrap/Button';
 
+import { useCookies } from 'react-cookie';
+
+import { useNavigate } from 'react-router-dom';
+
 import Login from './Login';
 import Register from './Register';
 
 const NavBar = () => {
+  const [cookies, setCookie] = useCookies(['username', 'user_id', 'logged_in']);
+  const username = cookies.username;
+  const logged_in = cookies.logged_in;
+  const user_id = cookies.user_id;
+
+  const navigate = useNavigate();
+
   const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
 
@@ -21,6 +32,13 @@ const NavBar = () => {
     <nav>
       <div className='logo'>SpotFinder</div>
 
+      { logged_in
+      ?
+        <>
+          <span>Welcome, {username}!</span>
+          <button type='submit' onClick={() => navigate(`/profile/${user_id}`)}>Profile</button>
+        </>
+      :
       <div className='navbar_btn_lp'>
         <Button className='login_and_register' onClick={handleLoginShow}>
           Login
@@ -32,6 +50,7 @@ const NavBar = () => {
         </Button>
         <Register handleClose={handleRegisterClose} show={showRegister}/>
       </div>
+      }
     </nav>
   );
 }
