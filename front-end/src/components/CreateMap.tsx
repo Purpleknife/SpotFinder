fa204ec29707b0da2ee7824ab487ccc0f6c75531
Hind <hind.nayhi@gmail.com>
2022-12-02 11:02:25 -0700

    Setup form to create a new map.

--- front-end/src/components/CreateMap.tsx
+++ front-end/src/components/CreateMap.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+
+import LocationList from './LocationList';
 
 
 interface CreateMapProps {
@@ -11,6 +14,59 @@ interface CreateMapProps {
 }
 
 const CreateMap = (props: CreateMapProps) => {
+  const [coordinates, setCoordinates] = useState<any>(null);
+  const [coordinatesList, setCoordinatesList] = useState<any>(null);
+
+  const getCoordinates = async() => {
+    return axios.get('/coordinates')
+      .then((res) => {
+        console.log('coordinates', res.data);
+        setCoordinates(res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  interface Coordinates {
+    id: number;
+    city: string;
+    province: string;
+    country: string;
+    latitude: number;
+    longitude: number;
+  };
+
+
+  const generateCoordinatesList = () => {
+    const coorList = coordinates.map((coor: Coordinates) => {
+      return (
+        <LocationList
+          key={coor.id}
+          id={coor.id}
+          city={coor.city}
+          province={coor.province}
+          country={coor.country}
+          latitude={coor.latitude}
+          longitude={coor.longitude}
+
+        />
+      )
+    });
+    setCoordinatesList(coorList);
+  };
+
+  useEffect(() => {
+    if (coordinates) {
+      generateCoordinatesList();
+    }
+  }, [coordinates]);
+
+
+  useEffect(() => {
+    getCoordinates();
+  }, []);
+
   return (
     <>
     <Modal show={props.show} onHide={props.handleClose} centered>
@@ -43,8 +99,7 @@ const CreateMap = (props: CreateMapProps) => {
               as='select'
             >
               <option value="Choose a province">Choose a province</option>
-              <option value="Alberta">Alberta</option>
-              <option value="Ontario">Ontario</option>
+              {coordinatesList}
             </Form.Control>
               
           </Form.Group>
