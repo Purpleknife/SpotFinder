4a4b03da9fa3d6f1922a6babf70338d7455873ec
Hind <hind.nayhi@gmail.com>
2022-12-02 12:15:34 -0700

    Move the logic that loads the coordinates to <App>.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,45 @@
+import React, { useEffect, useState } from 'react';
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
 import MapPage from './components/MapPage';
 import Profile from './components/Profile';
+import NavBar from './components/Navbar';
+
+import axios from 'axios';
 
 const App = () => {
+  const [coordinates, setCoordinates] = useState<any>(null);
+
+  const getCoordinates = async() => {
+    return axios.get('/coordinates')
+      .then((res) => {
+        console.log('coordinates', res.data);
+        setCoordinates(res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+
+  useEffect(() => {
+    getCoordinates();
+  }, []);
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/' element={
+          <>
+            <NavBar coordinates={coordinates} />
+            <LandingPage />
+          </>}
+        />
         <Route path='/maps/:map_id' element={<MapPage />} />
-        <Route path='/profile/:user_id' element={<Profile />} />
+        <Route path='/profile/:user_id' element={<><NavBar coordinates={coordinates} /><Profile /></>} />
       </Routes>
     </BrowserRouter>
   );
