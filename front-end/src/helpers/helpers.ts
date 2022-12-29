f3c6e6e679207121e50217e592c158fd3a830393
Hind <hind.nayhi@gmail.com>
2022-12-29 15:16:42 -0700

    Add helper function to scroll to top page.

--- front-end/src/helpers/helpers.ts
+++ front-end/src/helpers/helpers.ts
@@ -1,6 +1,9 @@
 /*
-Helper function that returns an array depending on the total of pages,
+
+createAPageArray: Helper function that returns an array depending on the total of pages,
 and the minimun page limit, which is normally 1.
+
+scrollToTopPage: To scroll to the top of the page when navigating the pages (pagination).
 */
 
 export const createAPageArray = (minPageLimit: number, totalPages: number): number[] => {
@@ -11,4 +14,13 @@ export const createAPageArray = (minPageLimit: number, totalPages: number): numb
   }
 
   return pages;
+};
+
+
+export const scrollToTopPage = (): void => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 };
