db1fd7daa04c7b4ddd337fb7b71e119898c02f77
Hind <hind.nayhi@gmail.com>
2022-12-29 15:17:12 -0700

    Setup pagination with ellipsis.

--- front-end/src/components/Pagination.tsx
+++ front-end/src/components/Pagination.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { createAPageArray } from '../helpers/helpers';
-
-// import './Pagination.scss';
+import { createAPageArray, scrollToTopPage } from '../helpers/helpers';
 
 interface PaginationProps {
   currentPage: number;
@@ -24,27 +22,53 @@ const Pagination = (props: PaginationProps) => {
     setMaxPageLimit(pages[pages.length - 1]);
   }, [pages]);
 
-  // Iterate through our pages array and return a list:
-  const pagesList = pages.map((page: number) => {
-    return (
-      <li 
-        key={page}
-        onClick={() => props.setCurrentPage(page)}
-      >
-        {page}
-      </li>
-    )
+
+  // pagesLeft are the pages that are not the 1st, last or 1st page between 2 page numbers.
+  let pagesLeft: boolean = false;
+
+
+  /* Iterate through our pages array and return a list of pages:
+    When isFirstPage, isLastPage and firstPageBetweenTwoNumbers are true,
+    it will show the page number. Otherwise, it will show ellipsis.
+  */
+
+  const pagesList = pages.map((page: number) => {    
+    const isFirstPage: boolean = page === 1;
+    const isLastPage: boolean = page === pages.length;
+    const firstPageBetweenTwoNumbers: boolean = Math.abs(page - props.currentPage) <= 1; // => If 1st page is 1 and last page is 5, this will be true for page 2.
+    
+    
+    if (isFirstPage || isLastPage || firstPageBetweenTwoNumbers) { 
+      return (
+        <li
+          key={page}
+          onClick={() => {props.setCurrentPage(page); scrollToTopPage();}}
+        >
+          {page}
+        </li>
+      )
+    }
+    
+    if (!pagesLeft) {
+      pagesLeft = true;
+      return <li key={page}>&hellip;</li>;
+    }
+    
   });
+
+
 
   const handlePrevClick = () => {
     if (props.currentPage !== 1) {
       props.setCurrentPage((prev: number) => prev - 1);
+      scrollToTopPage();
     }
   };
 
   const handleNextClick = () => {
     if (props.currentPage !== maxPageLimit) {
       props.setCurrentPage((prev: number) => prev + 1);
+      scrollToTopPage();
     }
   };
 
@@ -60,6 +84,7 @@ const Pagination = (props: PaginationProps) => {
           <button onClick={handleNextClick} disabled={props.currentPage === maxPageLimit}>Next</button>
         </li>
       </ul>
+      Page: {props.currentPage}
     </div>
   );
 }
