0f49e8c5e84eb58bcd6fb81dd304fcf24b596fe4
Hind <hind.nayhi@gmail.com>
2023-01-01 18:06:08 -0700

    Style the pins's likes.

--- front-end/src/components/PinsLikes.tsx
+++ front-end/src/components/PinsLikes.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import './MapLikes.scss';
+
+import moment from 'moment';
+
 interface PinsLikesProps {
   date_liked: string;
   first_name: string;
@@ -18,8 +22,16 @@ const PinsLikes = (props: PinsLikesProps) => {
   const navigate = useNavigate();
   
   return (
-    <div>
-      <p onClick={() => navigate(`/profile/${props.like_creator}`)}>{props.username}</p>
+    <div className='likes_list'>
+      <img
+        alt='like_img'
+        src={props.profile_image}
+      />
+      <div className='info'>
+      <span className='username' onClick={() => navigate(`/profile/${props.like_creator}`)}>{props.username}</span>
+      
+      <span className='date'><i className="fa-solid fa-calendar-days"></i> {moment(props.date_liked).format('LL')}</span>
+      </div>
     </div>
   );
 }
